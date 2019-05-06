93ce7e2bdbdbcd0b0acfd4d5d8ef464dfdf5d4af
Tri Hoang <trihoang@bu.edu>
2019-05-05 20:15:48 -0400

    template for interface

---
+++ rd_interface.h
@@ -0,0 +1,13 @@
+struct mode_t {
+    // TODO: Fill out mode struct
+};
+
+int rd_creat(char *pathname, mode_t mode);
+int rd_mkdir(char *pathname);
+int rd_open(char *pathname, int flags);
+int rd_close(int fd);
+int rd_read(int fd, char *address, int num_bytes);
+int rd_write(int fd, char *address, int num_bytes);
+int rd_lseek(int fd, int offset);
+int rd_unlink(char *pathname);
+int rd_chmod(char *pathname, mode_t mode);
