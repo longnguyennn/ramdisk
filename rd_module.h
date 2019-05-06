3d6c2a4131f0fee6ce9d945cd779f9a6212f4700
Long Nguyen <long.vnguyenn@gmail.com>
2019-05-06 02:46:52 -0400

    add func to get available memory block

--- rd_module.h
+++ rd_module.h
@@ -6,6 +6,8 @@
 #define MAX_PNAME_LENGTH 200
 #define NUM_DIRECT_BLOCK_PTR 8
 #define NUM_ENTRIES_PER_BLOCK 16
+#define BITMAP_ARR_LENGTH 1024
+#define INODE_BITMAP_LENGTH 240
 
 #define DIR_T 0
 #define REG_T 1
@@ -18,10 +20,12 @@
 #define CREAT _IOW(0, 2, char)
 #define MKDIR _IOW(0, 3, char)
 
+/* sizeof(superblock_t) = sizeof(unsigned long) * 2 + 240 = 256 */
 typedef struct {
 	unsigned long num_free_blocks;
 	unsigned long num_free_inodes;
-}__attribute__((aligned(128))) superblock_t;  // 128 byte alignment to make sizeof() = 256
+	char inode_bitmap[INODE_BITMAP_LENGTH];  // keep track of available inode
+} superblock_t;
 
 /* sizeof(inode_t) = 64 already, doesn't need to specify alignment */
 typedef struct {
@@ -33,7 +37,7 @@ typedef struct {
 
 /* sizeof(bitmap_t) = 1024 byte = 4 * (256 byte blocks) */
 typedef struct {
-	char array[1024];
+	char array[BITMAP_ARR_LENGTH];
 } bitmap_t;
 
 typedef struct {
@@ -49,3 +53,10 @@ typedef struct {
 typedef struct {
 	char path_name[MAX_PNAME_LENGTH];
 } mkdir_arg_t;
+
+/* function headers */
+dir_entry_t * find_file_entry_in_dir(inode_t *, char *, int *);
+inode_t * traverse(char *);
+int create_reg_file(inode_t *, char *, char);
+void * get_available_block();
+int get_available_inode_idx();
