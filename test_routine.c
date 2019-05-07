76d306d39871f97bf991a8247baf682b42b13c42
Tri Hoang <trihoang@bu.edu>
2019-05-07 11:51:06 -0400

    test routine that causes module crash

--- test_routine.c
+++ test_routine.c
@@ -4,22 +4,33 @@
 #include "rd_interface.h"
 
 #define MAX_FILES 1023
+#define RD  (S_IRUSR | S_IRGRP | S_IROTH)
+#define RW  (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
+#define WR  (S_IWUSR | S_IRGRP | S_IROTH)
 
 int main () {
 
 	int retval, i;
 	char pathname[80];
 
-	for (i = 0; i < MAX_FILES; i ++) {
-		sprintf(pathname, "/file%d", i);
-		retval = rd_creat(pathname, 0);
+	// for (i = 0; i < MAX_FILES; i ++) {
+	// 	sprintf(pathname, "/file%d", i);
+	// 	retval = rd_creat(pathname, 0);
 
-		if (retval < 0)
-			printf("ERROR! status: %d (%s)\n", retval, pathname);
-	}
+	// 	if (retval < 0)
+	// 		printf("ERROR! status: %d (%s)\n", retval, pathname);
+	// }
 
-	printf("SUCCESS\n");
+	char *pathname1 = "/hi";
+	char *pathname2 = "/hi/fi2";
+	retval = rd_mkdir(pathname1);
+	printf("Ret status: %d (%s)\n", retval, pathname1);
 
+	retval = rd_creat(pathname2, RW );
+	printf("Ret status: %d (%s)\n", retval, pathname2);
+
+	retval = rd_open(pathname2, 2 );
+	printf("Ret status: %d (%s)\n", retval, pathname2);
 
 	return 0;
 }
